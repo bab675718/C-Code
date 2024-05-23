2aa8bea7abd88b298e8f4546a3aaa3536b06be2d
bab675718 <aahmad675718@gmail.com>
2024-05-23 15:06:34 +0530

    add loop_quiz  file

---
+++ C++Code/loop_quiz.cpp
@@ -0,0 +1,14 @@
+//This is a infinite while loop
+//with black statement
+#include<iostream>
+using namespace std;
+
+int main(){
+	int i=0;
+
+	while(i<5);
+	{
+		cout<<"Arbab"<<endl;
+	}
+	return 0;
+}
