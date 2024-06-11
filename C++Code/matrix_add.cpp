cdd8b4e9b67a13a380b97f2946b8e745c89bd039
bab675718 <aahmad675718@gmail.com>
2024-06-11 20:22:39 +0530

    change matrix addition file

--- C++Code/matrix_add.cpp
+++ C++Code/matrix_add.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  int m = 3, n = 3;
-
-  int arr1[m][n];
-  int arr2[m][n];
-  int arr3[m][n];
+void matrix_addition(int **arr1, int **arr2, int m, int n) {
+  int *arr3[m];
 
   for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-      cin >> arr1[i][j];
-    }
-  }
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-      cin >> arr2[i][j];
-    }
+    arr3[i] = new int[n];
   }
 
   for (int i = 0; i < m; i++) {
@@ -31,6 +20,37 @@ int main() {
     }
     cout << endl;
   }
+  return;
+}
+int main() {
+  int m = 3, n = 3;
+
+  int **arr1;
+  int **arr2;
+
+  arr1 = new int *[m];
+  arr2 = new int *[m];
+
+  for (int i = 0; i < m; i++) {
+    arr1[i] = new int[n];
+  }
+
+  for (int i = 0; i < m; i++) {
+    arr2[i] = new int[n];
+  }
+
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> arr1[i][j];
+    }
+  }
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> arr2[i][j];
+    }
+  }
+
+  matrix_addition(arr1, arr2, m, n);
 
   return 0;
 }
